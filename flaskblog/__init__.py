ef77dcb2d6f7706b2980d0a105b732b9fd884142
cuongchu264 <cuongchu12369@gmail.com>
2022-01-05 14:40:05 +0700

    Update Flask Blog

---
+++ flaskblog/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bbdec85b1ea3a7cf1b3341bfde8de8e8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] =  'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEFAULT_SENDER'] = ('Display Name', 'noreply@insertdomain.com')
+app.config['MAIL_USERNAME'] = 'cuongchu264@gmail.com'
+app.config['MAIL_PASSWORD'] = 'matngua238057'
+
+mail = Mail(app)
+
+from flaskblog import routes
