fe2473e112d2efd25794fa37395f105a45522f45
cuongchu264 <cuongchu12369@gmail.com>
2022-01-17 11:26:38 +0700

    Fix SQLALCHEMY_TRACK_MODIFICATIONS Status

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bbdec85b1ea3a7cf1b3341bfde8de8e8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
